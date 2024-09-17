6536e05f07d1b3e0727f09ea16bf3eff2049388b
saurov-paul <saurov.paul1@gmail.com>
2024-09-17 14:26:32 +0300

    build and create react app with drupal

---
+++ web/themes/react_hello/js/src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+const root = ReactDom.createRoot(document.getElementById("react-app"));
+root.render(<h1>HEllo React - Drupal together</h1>);
+
+ 
